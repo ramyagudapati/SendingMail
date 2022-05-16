e671be784db60bf44935e695fee869555e9484ee
KodeCorp-PC <KodeCorp-PC@KODECORP>
2022-05-16 15:44:56 +0530

    Add project files.

---
+++ SendingMail/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SendingMail.MvcApplication" Language="C#" %>
---
+++ SendingMail/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SendingMail.Startup))]
+namespace SendingMail
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
